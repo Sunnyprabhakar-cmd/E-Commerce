67da3e1b6e387cafba4ff5bbce5e415ad66a0ddd
Sunny Prabhakar <anmsunnyprabhakar@gmail.com>
2026-06-25 15:03:33 +0530

    Fixed bug again+1 related to order

--- product_management_backend/database/database.js
+++ product_management_backend/database/database.js
@@ -3,15 +3,25 @@ dotenv.config();
 
 import pg from "pg";
 
-const useSsl = process.env.PG_SSL === 'true';
-const db = new pg.Client({
-  host: process.env.PG_HOST,
-  port: Number(process.env.PG_PORT),
-  user: process.env.PG_USER,
-  password: process.env.PG_PASSWORD,
-  database: process.env.PG_DATABASE,
-  ssl: useSsl ? { rejectUnauthorized: false } : false
-});
+const connectionString = process.env.DATABASE_URL;
+const explicitSsl = ['true', '1', 'yes'].includes(String(process.env.PG_SSL).toLowerCase());
+const connectionStringRequiresSsl = typeof connectionString === 'string' && /sslmode=require/i.test(connectionString);
+
+const dbConfig = connectionString
+  ? {
+      connectionString,
+      ssl: explicitSsl || connectionStringRequiresSsl ? { rejectUnauthorized: false } : false,
+    }
+  : {
+      host: process.env.PG_HOST,
+      port: Number(process.env.PG_PORT),
+      user: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DATABASE,
+      ssl: explicitSsl ? { rejectUnauthorized: false } : false,
+    };
+
+const db = new pg.Client(dbConfig);
 
 db.connect()
   .then(() => console.log("db connected"))
